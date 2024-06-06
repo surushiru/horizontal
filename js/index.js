7193acb1a84fdc546ee030e847454b18b9da3f57
surushiru <sugunitobu@gmail.com>
2024-06-06 23:14:31 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {
-    function checkOrientation() {
-        if (window.orientation === 0 || window.orientation === 180) {
-
-        } else if (window.orientation === 90) {
-            $(".img").addClass("a");
-            setTimeout(function() {
-                $(".img").removeClass("a");
-            }, 1000);
-        } else if (window.orientation === -90) {
-            $(".img").addClass("b");
-            setTimeout(function() {
-                $(".img").removeClass("b");
-            }, 1000);
+    function mobile(){
+        try{
+        document.createEvent("TouchEvent");
+        return true;
+        } catch(e) {
+        return false;
         }
-    }
-
-    // 初始檢查
-    checkOrientation();
-
-    // 偵測方向改變
-    $(window).on("orientationchange", function() {
+        }
+    if (mobile()) {
+        function checkOrientation() {
+            if (window.orientation === 0 || window.orientation === 180) {
+    
+            } else if (window.orientation === 90) {
+                $(".img").addClass("a");
+                setTimeout(function() {
+                    $(".img").removeClass("a");
+                }, 1000);
+            } else if (window.orientation === -90) {
+                $(".img").addClass("b");
+                setTimeout(function() {
+                    $(".img").removeClass("b");
+                }, 1000);
+            }
+        }
+    
+        // 初始檢查
         checkOrientation();
-    });
+    
+        // 偵測方向改變
+        $(window).on("orientationchange", function() {
+            checkOrientation();
+        });
+    }else{
+        $("body").html("スマホをご利用ください");
+    }
+    
 });
 
